0c628aea437442b2d0a926488babe8b8a6be8c46
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-14 20:03:54 -0700

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-import { loadGame, startGame, play } from './utilities.js';
+import {} from './logic.js';
 import { game } from './game.js';
 
+const startMenu = document.querySelector('.start-menu');
 const playButton = document.querySelector('#play-button');
+const optionsMenu = document.querySelector('.options-menu');
 const choices = document.querySelectorAll('.choices i');
+const playerHand = document.querySelector('.player-hand');
+const computerHand = document.querySelector('.computer-hand');
+const scoreboard = document.querySelector('.scoreboard');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
 
 playButton.addEventListener('click', startGame);
-choices.forEach((choice) => choice.addEventListener('click', play));
+choices.forEach((choice) =>
+  choice.addEventListener('click', () => playerChoice(choice.id))
+);
 
 loadGame();
+
+// utilities
+function loadGame() {
+  optionsMenu.style.display = 'none';
+  scoreboard.style.display = 'none';
+}
+
+function startGame() {
+  startMenu.style.display = 'none';
+  optionsMenu.style.display = 'flex';
+  scoreboard.style.display = 'grid';
+}
+
+function playerChoice(id) {
+  const playerChose = id;
+  console.log(playerChose);
+}
