c2216c04cdef7f6d7a2e6f08546cdcce4d645bce
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-14 19:17:28 -0700

    add querySelectors

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 import {} from './utilities.js';
 import { game } from './game.js';
+
+const startMenu = document.querySelector('.start-menu');
+const playButton = document.querySelector('#play-button');
+
+const optionsMenu = document.querySelector('.options-menu');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const playerHand = document.querySelector('.player-hand');
+const computerHand = document.querySelector('.computer-hand');
+
+const scoreboard = document.querySelector('.scoreboard');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+
+optionsMenu.style.display = 'none';
+scoreboard.style.display = 'none';
+playButton.addEventListener('click', () => console.log('playButton'));
