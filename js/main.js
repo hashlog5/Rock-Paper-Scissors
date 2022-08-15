2d0af38ba718f318fc9eceab5b6bb0c38e51043b
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-14 19:45:06 -0700

    addEventListener to buttons

--- js/main.js
+++ js/main.js
@@ -1,21 +1,10 @@
-import {} from './utilities.js';
+import { loadGame, startGame, play } from './utilities.js';
 import { game } from './game.js';
 
-const startMenu = document.querySelector('.start-menu');
 const playButton = document.querySelector('#play-button');
+const choices = document.querySelectorAll('.choices i');
 
-const optionsMenu = document.querySelector('.options-menu');
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
+playButton.addEventListener('click', startGame);
+choices.forEach((choice) => choice.addEventListener('click', play));
 
-const playerHand = document.querySelector('.player-hand');
-const computerHand = document.querySelector('.computer-hand');
-
-const scoreboard = document.querySelector('.scoreboard');
-const playerScore = document.querySelector('.player-score');
-const computerScore = document.querySelector('.computer-score');
-
-optionsMenu.style.display = 'none';
-scoreboard.style.display = 'none';
-playButton.addEventListener('click', () => console.log('playButton'));
+loadGame();
