2c6c7cee452d69c0f036269dab02af7425c34bb9
hashlog5 <108911663+hashlog5@users.noreply.github.com>
2022-08-17 19:44:22 -0700

    add utilities.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
 import { game } from './game.js';
-
-//! assets variables
-const ROCK_IMG = 'assets/rock.png';
-
-//! css variables
-const LIGHT_COLOR = 'var(--light-color)';
-const CHOICE_COLOR = 'var(--choice-color)';
-const PLAY_COLOR = 'var(--play-color)';
-const QUIT_COLOR = 'var(--quit-color)';
+import {
+  resetGameDisplay,
+  showFists,
+  resetWinnerStatus,
+  playGame,
+  disableButtons,
+  enableButtons,
+  animateHands,
+  showChoices,
+  setPlayerAsWinner,
+  setComputerAsWinner,
+  updatePlayerScore,
+  updateComputerScore,
+  setChoiceColor,
+  removeChoiceColor,
+} from './utilities.js';
 
 //! DOM
-const startMenu = document.querySelector('.start-menu');
 const play = document.querySelector('#play');
-
-const choicesMenu = document.querySelector('.choices-menu');
 const choices = document.querySelectorAll('.choices i');
-
-const playerHand = document.querySelector('.player-hand');
-const computerHand = document.querySelector('.computer-hand');
-
-const scoreboard = document.querySelector('.scoreboard');
-const player = document.querySelector('.player');
-const computer = document.querySelector('.computer');
-const playerScore = document.querySelector('.player-score');
-const computerScore = document.querySelector('.computer-score');
-
-const quitMenu = document.querySelector('.quit-menu');
 const quit = document.querySelector('#quit');
 
 //! events
@@ -43,96 +36,11 @@ quit.addEventListener('click', resetGame);
 //! app
 resetGame();
 
-//! utilities
 function resetGame() {
   resetGameDisplay();
   showFists();
   resetWinnerStatus();
   game.resetScores();
-}
-
-function resetGameDisplay() {
-  startMenu.style.display = 'flex';
-  choicesMenu.style.display = 'none';
-
-  scoreboard.classList.remove('show-scoreboard');
-  playerScore.textContent = 0;
-  computerScore.textContent = 0;
-
-  quitMenu.classList.remove('show-quit-menu');
-}
-
-function showFists() {
-  playerHand.src = ROCK_IMG;
-  computerHand.src = ROCK_IMG;
-}
-
-function resetWinnerStatus() {
-  player.style.color = LIGHT_COLOR;
-  player.style.fontWeight = 'normal';
-
-  computer.style.color = LIGHT_COLOR;
-  computer.style.fontWeight = 'normal';
-}
-
-function playGame() {
-  disableButtons();
-
-  startMenu.style.display = 'none';
-  choicesMenu.style.display = 'flex';
-  scoreboard.classList.add('show-scoreboard');
-  quitMenu.classList.add('show-quit-menu');
-
-  setTimeout(enableButtons, 500);
-}
-
-function disableButtons() {
-  choicesMenu.style.pointerEvents = 'none';
-  quitMenu.style.pointerEvents = 'none';
-}
-
-function enableButtons() {
-  choicesMenu.style.pointerEvents = 'all';
-  quitMenu.style.pointerEvents = 'all';
-}
-
-function animateHands() {
-  playerHand.style.animation = 'shakePlayerHand 1.75s ease';
-  computerHand.style.animation = 'shakeComputerHand 1.75s ease';
-
-  setTimeout(() => {
-    playerHand.style.animation = '';
-    computerHand.style.animation = '';
-  }, 1500);
-}
-
-function showChoices(playerChoice, computerChoice) {
-  playerHand.src = `assets/${playerChoice}.png`;
-  computerHand.src = `assets/${computerChoice}.png`;
-}
-
-function setPlayerAsWinner() {
-  player.style.color = PLAY_COLOR;
-  player.style.fontWeight = 'bold';
-
-  computer.style.color = QUIT_COLOR;
-  computer.style.fontWeight = 'normal';
-}
-
-function setComputerAsWinner() {
-  computer.style.color = PLAY_COLOR;
-  computer.style.fontWeight = 'bold';
-
-  player.style.color = QUIT_COLOR;
-  player.style.fontWeight = 'normal';
-}
-
-function setChoiceColor(choice) {
-  choice.style.color = CHOICE_COLOR;
-}
-
-function removeChoiceColor(choice) {
-  choice.style.color = '';
 }
 
 //! logic
@@ -189,14 +97,13 @@ function updateScores(winner) {
     case 'player':
       game.playerEarnPoints();
       setPlayerAsWinner();
-      playerScore.textContent = game.playerScore;
-
+      updatePlayerScore(game.playerScore);
       break;
 
     case 'computer':
       game.computerEarnPoints();
       setComputerAsWinner();
-      computerScore.textContent = game.computerScore;
+      updateComputerScore(game.computerScore);
       break;
   }
 }
---
+++ js/utilities.js
@@ -0,0 +1,115 @@
+//! assets variables
+const ROCK_IMG = 'assets/rock.png';
+
+//! css variables
+const LIGHT_COLOR = 'var(--light-color)';
+const CHOICE_COLOR = 'var(--choice-color)';
+const PLAY_COLOR = 'var(--play-color)';
+const QUIT_COLOR = 'var(--quit-color)';
+
+//! DOM
+const startMenu = document.querySelector('.start-menu');
+const choicesMenu = document.querySelector('.choices-menu');
+const quitMenu = document.querySelector('.quit-menu');
+
+const playerHand = document.querySelector('.player-hand');
+const computerHand = document.querySelector('.computer-hand');
+
+const scoreboard = document.querySelector('.scoreboard');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
+
+//! utilities
+export function resetGameDisplay() {
+  startMenu.style.display = 'flex';
+  choicesMenu.style.display = 'none';
+
+  scoreboard.classList.remove('show-scoreboard');
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+
+  quitMenu.classList.remove('show-quit-menu');
+}
+
+export function showFists() {
+  playerHand.src = ROCK_IMG;
+  computerHand.src = ROCK_IMG;
+}
+
+export function resetWinnerStatus() {
+  player.style.color = LIGHT_COLOR;
+  player.style.fontWeight = 'normal';
+
+  computer.style.color = LIGHT_COLOR;
+  computer.style.fontWeight = 'normal';
+}
+
+export function playGame() {
+  disableButtons();
+
+  startMenu.style.display = 'none';
+  choicesMenu.style.display = 'flex';
+  scoreboard.classList.add('show-scoreboard');
+  quitMenu.classList.add('show-quit-menu');
+
+  setTimeout(enableButtons, 500);
+}
+
+export function disableButtons() {
+  choicesMenu.style.pointerEvents = 'none';
+  quitMenu.style.pointerEvents = 'none';
+}
+
+export function enableButtons() {
+  choicesMenu.style.pointerEvents = 'all';
+  quitMenu.style.pointerEvents = 'all';
+}
+
+export function animateHands() {
+  playerHand.style.animation = 'shakePlayerHand 1.75s ease';
+  computerHand.style.animation = 'shakeComputerHand 1.75s ease';
+
+  setTimeout(() => {
+    playerHand.style.animation = '';
+    computerHand.style.animation = '';
+  }, 1500);
+}
+
+export function showChoices(playerChoice, computerChoice) {
+  playerHand.src = `assets/${playerChoice}.png`;
+  computerHand.src = `assets/${computerChoice}.png`;
+}
+
+export function setPlayerAsWinner() {
+  player.style.color = PLAY_COLOR;
+  player.style.fontWeight = 'bold';
+
+  computer.style.color = QUIT_COLOR;
+  computer.style.fontWeight = 'normal';
+}
+
+export function setComputerAsWinner() {
+  computer.style.color = PLAY_COLOR;
+  computer.style.fontWeight = 'bold';
+
+  player.style.color = QUIT_COLOR;
+  player.style.fontWeight = 'normal';
+}
+
+export function updatePlayerScore(playerPoints) {
+  playerScore.textContent = playerPoints;
+}
+
+export function updateComputerScore(computerPoints) {
+  computerScore.textContent = computerPoints;
+}
+
+export function setChoiceColor(choice) {
+  choice.style.color = CHOICE_COLOR;
+}
+
+export function removeChoiceColor(choice) {
+  choice.style.color = '';
+}
